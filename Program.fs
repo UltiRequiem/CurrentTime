134098271e1efd0da09e8f2c491488707758fb9d
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-07-26 11:07:12 -0500

    :recycle: Update

--- Program.fs
+++ Program.fs
@@ -1,4 +1,20 @@
 ﻿open System
+open Humanizer
+
 let timeNow () = DateTime.Now.ToLongTimeString()
 
-printfn $"The time now is %s{timeNow()}"
+let currentWeek () = Globalization.ISOWeek.GetWeekOfYear(DateTime.Now).ToWords()
+
+let currentMonth () =DateTime.Now.Month.ToWords()
+
+let currentYear () = DateTime.Now.Year.ToWords()
+
+let currentDay () = DateTime.Now.Day.ToWords()
+
+printfn $"The time now is %s{timeNow()}."
+
+printfn $"We are in the day %s{currentDay()} of the month %s{currentMonth()} of the year %s{currentYear()}."
+
+printfn $">> %s{DateTime.Now.ToOrdinalWords()}"
+
+printfn "Cheers."
